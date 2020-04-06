0a6f5070dab6d336ffd9606b5bf1ca99a1970dcf
Djotchuang Tamo Karl Wilfried <djotchuangtamo@gmail.com>
2020-04-06 13:25:34 +0100

    Second commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+ScrollReveal({ reset: true, duration: 1500 });
+
+ScrollReveal().reveal(".features .card");
+ScrollReveal().reveal(".site-content .d-flex");
+ScrollReveal().reveal(".section-2 .d-flex");
+ScrollReveal().reveal(".section-3 .col-md-4");
+ScrollReveal().reveal(".section-4 .col-md-7, .col-md-5");
+ScrollReveal().reveal("#gallery");
