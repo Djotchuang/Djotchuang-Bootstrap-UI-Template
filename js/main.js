816020af11fd49b803cb592409935e62e4cdb5a4
Djotchuang Tamo Karl Wilfried <djotchuangtamo@gmail.com>
2020-04-07 05:01:18 +0100

    Added Smooth Scroll functionality

--- js/main.js
+++ js/main.js
@@ -6,3 +6,22 @@ ScrollReveal().reveal(".section-2 .d-flex");
 ScrollReveal().reveal(".section-3 .col-md-4");
 ScrollReveal().reveal(".section-4 .col-md-7, .col-md-5");
 ScrollReveal().reveal("#gallery");
+
+// Smooth Scrolling
+$(".cf a").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      4400,
+      function () {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
